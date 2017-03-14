1b934573aba92f36532bac6e38a61df389efcba7
Thomas Lee <thomasgylee@gmail.com>
2017-03-14 00:04:27 -0400

    Better formatting for mod-inspector output

--- mod-inspector.py
+++ mod-inspector.py
@@ -16,24 +16,24 @@ SORT_BY_NAME = True
 import os, glob
 
 ws_mods_path = os.path.join(STEAM_PATH, WS_SUB_DIR, X2_ID, '*')
-mods = glob.glob(ws_mods_path)
+mod_paths = glob.glob(ws_mods_path)
 
 mapping = {}
-for mod in mods:
-    id = os.path.basename(mod)
-    mod_file = glob.glob(os.path.join(mod, '*.XComMod'))
+for mod_path in mod_paths:
+    mod_id = os.path.basename(mod_path)
+    mod_file = glob.glob(os.path.join(mod_path, '*.XComMod'))
     if mod_file:
         with open(mod_file[0], 'r') as file:
             data = {}
             for line in file.readlines():
                 if line.startswith('Title'):
                     title = ''.join(line.split('=')[1:]).strip()
-                    mapping[id] = title
+                    mapping[mod_id] = title
                     break
 
 mapping = sorted(mapping.items())
 if SORT_BY_NAME:
     mapping = sorted(mapping, key=lambda item: item[1])
 
-for key, value in mapping:
-    print(key, value)
+for mod_id, name in mapping:
+    print('{id} {name}'.format(id=mod_id, name=name))
